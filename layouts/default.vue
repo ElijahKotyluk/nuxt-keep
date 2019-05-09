717dbf2364b157915039f0ddc6680231ad34e076
Elijah Kotyluk <elijah@elijahkotyluk.com>
2019-05-09 11:11:32 -0700

    Initialize repository:  - Remove default template from cli  - Install firebase && @nuxtjs/dotenv  - Add firebase plugin that initializes firebase  - Add a .dotenv.example file

---
+++ layouts/default.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-app dark>
+    <v-toolbar
+      :clipped-left="clipped"
+      fixed
+      app
+    >
+      <v-spacer />
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+    </v-toolbar>
+
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+
+    <v-footer
+      :fixed="fixed"
+      app
+    >
+      <span>&copy; 2019</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'apps',
+          title: 'Welcome',
+          to: '/'
+        },
+        {
+          icon: 'bubble_chart',
+          title: 'Inspire',
+          to: '/inspire'
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Nuxt Keep'
+    }
+  }
+}
+</script>
